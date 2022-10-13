09f5a0cfe2479c89a06ad21d4560a04f32a1687c
st087920 <114222786+st087920@users.noreply.github.com>
2022-10-13 20:42:01 +0300

    Update server.py

--- server.py
+++ server.py
@@ -2,17 +2,16 @@
 import socket
 
 
-if __name__ == '__main__':
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind(('0.0.0.0', 2222))
-    s.listen(10)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('0.0.0.0', 2222))
+s.listen(10)
+while True:
+    conn, addr = s.accept()
     while True:
-        conn, addr = s.accept()
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            if data.decode() == "close":
-                break
-            conn.send(data)
+        data = conn.recv(1024)
+        if not data:
+            break
+        if data.decode() == "close":
+            break
+        conn.send(data)
         conn.close()
