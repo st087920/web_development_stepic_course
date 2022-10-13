9e25bbb5fad8c8e16c38b0cebd4ae62fd15330cb
st087920 <114222786+st087920@users.noreply.github.com>
2022-10-13 20:49:05 +0300

    another try

--- server.py
+++ server.py
@@ -7,11 +7,9 @@ s.bind(('0.0.0.0', 2222))
 s.listen(10)
 while True:
     conn, addr = s.accept()
-    while True:
-        data = conn.recv(1024)
-        if not data:
-            break
-        if data.decode() == "close":
-            break
+    data = conn.recv(1024)
+    if (not data) or (data.decode() == "close"):
+        conn.close()
+        break
     conn.send(data)
     conn.close()
