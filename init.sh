9e77299ecd67bb2e22b5d89a5ed7d35e5c31f196
st087920 <114222786+st087920@users.noreply.github.com>
2022-10-14 15:10:11 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
+sudo gunicorn -b 0.0.0.0:8080 hello:wsgi_aplication
