49a3a35a48d1134669e21a3e91a1e7b00c0a47b8
st087920 <114222786+st087920@users.noreply.github.com>
2022-10-14 13:46:38 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,2 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo rm -rf /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
