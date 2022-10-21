fa37bfd38f49fc094234bfcea3a39f9bacd7d300
st087920 <114222786+st087920@users.noreply.github.com>
2022-10-21 23:22:24 +0300

    Update urls.py

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -4,11 +4,11 @@ from django.contrib import url
 from django.urls import path
 
 urlpatterns = [
-    url(r'^*', test, name='home'),
-    url(r'^question/(\d+)/*', test, name='question'),
-    url(r'^login/*', test, name='login'),
-    url(r'^signup/*', test, name='signup'),
-    url(r'^ask/*', test, name='ask'),
-    url(r'^popular/*$', test, name='popular'),
-    url(r'^new/*$', test, name='new'),
+    url(r'.*', test, name='home'),
+    #url(r'^question/(\d+)/*', test, name='question'),
+    #url(r'^login/*', test, name='login'),
+    #url(r'^signup/*', test, name='signup'),
+    #url(r'^ask/*', test, name='ask'),
+    #url(r'^popular/*$', test, name='popular'),
+    #url(r'^new/*$', test, name='new'),
 ]
