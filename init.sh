ace13f02c5d88b28a47cf073588e436e2bbb7549
st087920 <114222786+st087920@users.noreply.github.com>
2022-10-21 22:35:09 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,6 +5,8 @@ sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
 cd /home/box/web/
 gunicorn --bind='0.0.0.0:8080' hello:wsgi_aplication
+sudo ln -sf /home/box/web/etc/ask_conf.py /etc/gunicorn.d/ask_conf.py
+sudo gunicorn -c /etc/gunicorn.d/ask_conf.py ask.wsgi:application
 
 #sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
 #sudo /etc/init.d/gunicorn restart
