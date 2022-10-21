1ab2d9d9d2dd82ae52f9cc429f42951a13ff4766
st087920 <114222786+st087920@users.noreply.github.com>
2022-10-21 23:24:28 +0300

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -19,10 +19,10 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^$', include('qa.urls')),
-    url(r'^question/(\d+)/$', include('qa.urls')),
-    url(r'^login/$', include('qa.urls')),
-    url(r'^signup/$', include('qa.urls')),
-    url(r'ask/^$', include('qa.urls')),
-    url(r'popular/^$', include('qa.urls')),
-    url(r'new/^$', include('qa.urls')),
+    url(r'^question/(\d+)/.*', include('qa.urls')),
+    url(r'^login/.*', include('qa.urls')),
+    url(r'^signup/.*', include('qa.urls')),
+    url(r'^ask/*', include('qa.urls')),
+    url(r'^popular/*', include('qa.urls')),
+    url(r'^new/.*', include('qa.urls')),
 ]
