65fc40e12bc12912fce097a5d36148d7a5dc10d1
st087920 <114222786+st087920@users.noreply.github.com>
2022-10-26 12:36:15 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,10 +3,10 @@ sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
 cd /home/box/web
-sudo gunicorn -b 0.0.0.0:8080 hello:wsgi_aplication &
+sudo gunicorn -b 0.0.0.0:8080 hello:wsgi_aplication
 
 sudo ln -sf /home/box/web/etc/ask_conf.py /etc/gunicorn.d/ask_conf.py
-sudo gunicorn -c /etc/gunicorn.d/ask_conf.py ask.wsgi:application &
+sudo gunicorn -c /etc/gunicorn.d/ask_conf.py ask.wsgi:application
 
 # sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 # sudo /etc/init.d/gunicorn restart
