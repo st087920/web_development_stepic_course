315da5526313abe56393a98aa005fd70589a7c52
st087920 <114222786+st087920@users.noreply.github.com>
2022-10-26 12:35:27 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart &
+sudo /etc/init.d/nginx restart
 
 cd /home/box/web
 sudo gunicorn -b 0.0.0.0:8080 hello:wsgi_aplication &
